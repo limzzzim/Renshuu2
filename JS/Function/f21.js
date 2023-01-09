aac8567380bed9a20ed3ad96fe5e5772924612bd
limzzzim <qnhuhbt@gmail.com>
2023-01-09 19:09:09 +0700

    update

---
+++ JS/Function/f21.js
@@ -0,0 +1,14 @@
+function isPrime(number) {
+    let start = 2;
+    if (number <= 1) {
+        console.log(`${number} Không phải là số nguyên tố`);
+    }
+    if (start <= number - 1) {
+        if (number % start == 0) {
+            console.log(`${number} Không phải là số nguyên tố`);;
+        } else {
+            console.log(`${number} Là số nguyên tố`);;
+        }
+    }
+}
+isPrime(89);
